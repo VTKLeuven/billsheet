e2fb6a7b9234a545cae62eca999a2dd832c7d9f9
Robbe Serry <robbeserry@gmail.com>
2024-01-12 22:26:05 +0100

    add logout button

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,6 +2,8 @@ import { useUser } from "@supabase/auth-helpers-react"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
 import getUserData from "../lib/getUser";
+import { supabase } from "../lib/supabaseClient";
+import { notifications } from "@mantine/notifications";
 
 export default function NavBar() {
     const router = useRouter();
@@ -25,6 +27,16 @@ export default function NavBar() {
 
     if (admin) {
         links.set("Admin", "/admin");
+    } else {
+        links.delete("Admin")
+    }
+
+    async function logOut() {
+        const { error } = await supabase.auth.signOut()
+        notifications.show({
+            title: "Logged out",
+            message: "You have been logged out"
+        })
     }
 
     return (
@@ -36,6 +48,11 @@ export default function NavBar() {
                     </div>
                 </button>
             )}
+            {user ? <button className="float-right" onClick={() => logOut()}>
+                <div className="p-4 text-xl font-bold b-x-2 border-slate-600">
+                    Logout
+                </div>
+            </button> : <></>}
         </div>
     )
 }
