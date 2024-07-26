76cb672b02027404a75c24aa56d15da14beea13c
Robbe Serry <robbeserry@gmail.com>
2024-07-26 21:09:07 +0200

    remove download size limit

--- pages/api/downloadReport.ts
+++ pages/api/downloadReport.ts
@@ -130,5 +130,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 }
 
 export const config = {
-    api: { bodyParser: false, },
+    api: {
+        bodyParser: false,
+        responseLimit: false,
+    },
 };
